68df93361993fddf9bce9080a23d6cd9d4237437
Footconnect451 <maillotcenter@protonmail.com>
2026-03-07 20:39:45 +0400

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://ebisacqpllyljnyjjhek.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_JIK7kSNVQt5tYYWZtgiaRA_80QgSMpj'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
